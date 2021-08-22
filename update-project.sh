3ef553e62a2cf53df1ab0db4fdfd8b0cd66a70de
Paul Chaignon <paul@cilium.io>
2021-08-22 20:31:09 +0200

    f
    Signed-off-by: Paul Chaignon <paul@cilium.io>

--- update-project.sh
+++ update-project.sh
@@ -1,9 +1,24 @@
 #!/bin/bash
 issue=$1
-src_col_name=$2
-dst_col_name=$3
+label=$2
+
+if [[ "$label" != status/* ]]; then
+	exit 0
+fi
+
 project=$(curl -su pchaigno:$TOKEN -H "Accept: application/vnd.github.inertia-preview+json" https://api.github.com/repos/pchaigno/tmp/projects | jq '.[] | select(.name=="Test").id')
-src_column=$(curl -su pchaigno:$TOKEN -H "Accept: application/vnd.github.inertia-preview+json" https://api.github.com/projects/$project/columns | jq ".[] | select(.name==\"$src_col_name\").id")
-dst_column=$(curl -su pchaigno:$TOKEN -H "Accept: application/vnd.github.inertia-preview+json" https://api.github.com/projects/$project/columns | jq ".[] | select(.name==\"$dst_col_name\").id")
-card=$(curl -su pchaigno:$TOKEN -H "Accept: application/vnd.github.inertia-preview+json" https://api.github.com/projects/columns/$src_column/cards | jq ".[] | select(.content_url | endswith(\"/$issue\")).id")
-curl -u pchaigno:$TOKEN -X POST -H "Accept: application/vnd.github.inertia-preview+json" https://api.github.com/projects/columns/cards/$card/moves -d "{\"position\":\"top\",\"column_id\":$dst_column}"
+columns=$(curl -su pchaigno:$TOKEN -H "Accept: application/vnd.github.inertia-preview+json" https://api.github.com/projects/$project/columns | jq ".[].id")
+dst_col_name=${label#"status/"}
+for col in ${columns[@]}; do
+	dst_column=$(curl -su pchaigno:$TOKEN -H "Accept: application/vnd.github.inertia-preview+json" https://api.github.com/projects/$project/columns | jq ".[] | select(.name|ascii_downcase==\"$dst_col_name\").id")
+	if [ "$dst_column" == "" ]; then
+		echo "No project column found for label $label."
+		exit 1
+	fi
+	card=$(curl -su pchaigno:$TOKEN -H "Accept: application/vnd.github.inertia-preview+json" https://api.github.com/projects/columns/$col/cards | jq ".[] | select(.content_url | endswith(\"/$issue\")).id")
+	if [ "$card" != "" ] && [ "$col" != "$dst_column" ]; then
+		echo "Moving issue to appropriate column."
+		curl -u pchaigno:$TOKEN -X POST -H "Accept: application/vnd.github.inertia-preview+json" https://api.github.com/projects/columns/cards/$card/moves -d "{\"position\":\"top\",\"column_id\":$dst_column}"
+		break
+	fi
+done
